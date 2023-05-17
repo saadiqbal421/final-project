4d2be4abc92496118718acf90587c1902a46927c
Saad Iqbal Chaudhry <saadiqbalch@gmail.com>
2023-05-17 08:46:10 +0500

    last comit

--- client/client/pages/createnewpackage.js
+++ client/client/pages/createnewpackage.js
@@ -4,6 +4,10 @@ import { toast } from "react-toastify";
 import Link from "next/link";
 import { Context } from "../context";
 import { useRouter } from "next/router";
+import { createContext } from "react";
+
+
+
 
 const Form = () => {
   const [values, setValues] = useState({
@@ -11,13 +15,12 @@ const Form = () => {
     days: "",
     price: "",
     description: "",
-    image: "",
+    image: null,
     loading: false,
   });
 
   const { state, dispatch } = useContext(Context);
 
-  const [image, setImage] = useState({});
   const [preview, setPreview] = useState("");
   const [uploadButtonText, setUploadButtonText] = useState("Upload Image");
 
@@ -29,10 +32,14 @@ const Form = () => {
     }
   }, [pack]);
 
-  const { place, days, price, description } = values;
+  const { place, days, price, description, image } = values;
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+    if (!place || !days || !price || !description) {
+      toast.error("Please enter all the data.");
+      return;
+    }
+
     try {
       setValues({ ...values, loading: true });
       const formData = new FormData();
@@ -41,49 +48,58 @@ const Form = () => {
       formData.append("price", price);
       formData.append("description", description);
       formData.append("image", image);
-  
-      const { data } = await axios.post(`/api/createnewpackage`, formData);
+
+      await axios.post("/api/createnewpackage", formData);
       toast.success("Submitted");
-  
+
       console.log("Submit response", { data });
-  
+
       dispatch({
         type: "Submit",
         payload: data,
       });
-  
+
       window.localStorage.setItem("createnewpackage", JSON.stringify(data));
-  
+
       // router.push("/user");
     } catch (err) {
-      const errorMessage = err.response
-        ? err.response.data
-        : "Error saving data";
-      toast.error(errorMessage);
+      const errorMessage = err.response ? err.response.data : "Error saving data";
+     // toast.error(errorMessage);
     }
   };
 
   const handlePlaceChange = (e) => {
     const inputValue = e.target.value;
     const pattern = /^[a-zA-Z\s]*$/; // Regular expression pattern for alphabets and spaces only
-  
+
     if (pattern.test(inputValue)) {
       setValues({ ...values, place: inputValue });
-       // Clear the error message when the input is correct
+      // Clear the error message when the input is correct
+    } else {
+      toast.error("Invalid input. Only alphabets and spaces are allowed.");
     }
- 
-  else 
-  {
-    toast.error(errorMessage);
-  }
   };
 
   const handleDaysChange = (e) => {
-    setValues({ ...values, days: e.target.value });
+    const inputValue = e.target.value;
+    const pattern = /^[0-9\b]+$/; // Regular expression pattern for numbers only
+
+    if (pattern.test(inputValue)) {
+      setValues({ ...values, days: inputValue });
+    } else {
+      toast.error("Invalid input. Only numbers are allowed.");
+    }
   };
 
   const handlePriceChange = (e) => {
-    setValues({ ...values, price: e.target.value });
+    const inputValue = e.target.value;
+    const pattern = /^[0-9\b]+$/; // Regular expression pattern for numbers only
+
+    if (pattern.test(inputValue)) {
+      setValues({ ...values, price: inputValue });
+    } else {
+      toast.error("Invalid input. Only numbers are allowed.");
+    }
   };
 
   const handleDescriptionChange = (e) => {
@@ -91,66 +107,70 @@ const Form = () => {
   };
 
   const handleImageChange = (e) => {
-    setImage(e.target.files[0]);
-    setPreview(URL.createObjectURL(e.target.files[0]));
-    setUploadButtonText(e.target.files[0].name);
+    const selectedImage = e.target.files[0];
+
+
+
   };
-  
 
     return (
+      <FormDataContext.Provider value={values}>
     <form className="form" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="place">Place:</label>
-        <input
-          type="text"
-          id="place"
-          name="place"
-       
-          onChange={handlePlaceChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="days">Days:</label>
-        <input
-          type="text"
-          id="days"
-          name="days"
-          onChange={handleDaysChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="price">Price:</label>
-        <input
-          type="text"
-          id="price"
-          name="price"
-          onChange={handlePriceChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="description">Description:</label>
-        <textarea
-          id="description"
-          name="description"
-          onChange={handleDescriptionChange}
- 
-        ></textarea>
-      </div>
-      <div>
-        <label htmlFor="image">Image:</label>
-        <input
-          type="file"
-          id="image"
-          name="image"
-          accept="image/*"
-          onChange={handleImageChange}
-        />
-      </div>
-      <button type="submit">Submit</button>
+    <div>
+    <label htmlFor="place">Place:</label>
+    <input
+           type="text"
+           id="place"
+           name="place"
+           value={values.place}
+           onChange={handlePlaceChange}
+         />
+    </div>
+    <div>
+    <label htmlFor="days">Days:</label>
+    <input
+           type="text"
+           id="days"
+           name="days"
+           value={values.days}
+           onChange={handleDaysChange}
+         />
+    </div>
+    <div>
+    <label htmlFor="price">Price:</label>
+    <input
+           type="text"
+           id="price"
+           name="price"
+           value={values.price}
+           onChange={handlePriceChange}
+         />
+    </div>
+    <div>
+    <label htmlFor="description">Description:</label>
+    <textarea
+           id="description"
+           name="description"
+           value={values.description}
+           onChange={handleDescriptionChange}
+         ></textarea>
+    </div>
+    <div>
+    <label htmlFor="image">Image:</label>
+    <input
+           type="file"
+           id="image"
+           name="image"
+           accept="image/*"
+           value={values.image}
+           onChange={handleImageChange}
+         />
+    </div>
+    <button type="submit" onClick={submit}>Submit</button>
     </form>
-    
-  );
-  
-};
+    </FormDataContext.Provider>
+    );
+    };
 
-export default Form;
+    export const FormDataContext = createContext(null);
+    export default Form;
--- client/client/pages/offeredpackages.js
+++ client/client/pages/offeredpackages.js
@@ -1,27 +1,19 @@
-import React from 'react';
+import { useContext } from "react";
+import { FormDataContext } from "./Form";
 
-const NextPage = ({ formData }) => {
-  if (!formData || Object.keys(formData).length === 0) {
-    return (
-      <div>
-        <h1>No data submitted</h1>
-      </div>
-    );
-  }
+const DisplayFormData = () => {
+  const formData = useContext(FormDataContext);
 
   return (
     <div>
-      <h1>Submitted Data</h1>
-      <div>
-        <h3>Place: {formData.place}</h3>
-        <h3>Days: {formData.days}</h3>
-        <h3>Price: {formData.price}</h3>
-        <h3>Description: {formData.description}</h3>
-        {/* Render the image using formData.image */}
-        <img src={formData.image} alt="Submitted" />
-      </div>
+      <h2>Form Data:</h2>
+      <p>Place: {formData.place}</p>
+      <p>Days: {formData.days}</p>
+      <p>Price: {formData.price}</p>
+      <p>Description: {formData.description}</p>
+      <p>Image: {formData.image ? formData.image.name : "No image selected"}</p>
     </div>
   );
 };
 
-export default NextPage;
+export default DisplayFormData;
